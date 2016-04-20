533c2c16fc9c9549e570b7ce95eceeafb051649e
Pete Allport <itspeteallport@gmail.com>
2016-04-19 21:08:37 -0400

    updates existingComponent

--- src/components/existingComponent/index.js
+++ src/components/existingComponent/index.js
@@ -3,11 +3,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './existingComplaint.scss';
 
-class existingComplaint extends React.Component {
+class existingComplaint extends Component {
+  constructor(props) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick(event) {
+    alert('hi');
+    console.log('hello')
+  };
+
   render() {
     return (
       <div className='existingComplaint'>
-        <div className='complaintVotes'><span className='voteEmoji'>😠</span> 7<div className='voteText'>Upvotes</div></div>
+        <div onClick={() => alert('test')} className='complaintVotes'><span className='voteEmoji'>😠</span> {this.props.votes}<div className='voteText'>Upvotes</div></div>
         <div className='complaintTitle'><strong>title of message{this.props.title}</strong></div>
         <div className='complaintBody'>body of message{this.props.body}</div>
         <div className='complaintInfo'>Apr 19th, 2016 - Winter Park, FL</div>
